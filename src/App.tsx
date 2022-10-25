7d09d7a9168d2fb016f857e2c39504b5be9da1d0
inrongpon22 <inrongpon.phanupong2@gmail.com>
2022-10-25 14:48:37 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+//Pictures
+import avatar from "./assets/images/avatar.jpg";
+import Pic1 from "./assets/images/dog-image-1.jpg";
+import Pic2 from "./assets/images/dog-image-2.jpg";
+import Pic3 from "./assets/images/dog-image-3.jpg";
+
+// Icons
+import { GrPrevious, GrFormNext } from "react-icons/gr";
+import { FiMoreVertical } from "react-icons/fi";
+
+function App() {
+  const style = { color: "white" }
+
+  return (
+    <main className="App relative">
+      <div className="relative overflow-x-hidden overflow-y-hidden">
+        <div className="z-20 m-5 flex flex-col lg:flex-row justify-center items-center">
+          <section className="z-10 w-11/12 sm:w-[19.5rem] md:w-[19.5rem] h-[41.5rem] shadow-lg rounded-[2.5rem] overflow-hidden bg-white">
+            <div className="w-[18rem] h-[40rem] mx-auto mt-3 bg-[#F5F3F7] rounded-[1.5rem]">
+              <div className="w-[18rem] h-[5rem] relative bg-gradient-to-l from-[#E942FF] to-[#8838FF] rounded-t-[1.5rem] rounded-b-lg">
+                <div className="absolute w-[10rem] h-[2.5rem] bg-white rounded-[2rem] left-[4rem] -top-[1.3rem]"></div>
+                <div className="flex justify-between items-center text-white font-rubik pt-6 px-3">
+                  <div className="flex items-center">
+                    <GrPrevious color="white" fill="white" />
+                    <img
+                      src={avatar}
+                      alt=""
+                      className="w-8 h-8 ml-2 rounded-full border-2"
+                    />
+                    <div className="ml-2">
+                      <p className="font-bold">Samuel Green</p>
+                      <p className="text-xs text-[#A39DAA]">Available to Walk</p>
+                    </div>
+                  </div>
+                  <FiMoreVertical />
+                </div>
+              </div>
+              <p className="w-2/3 ml-3 mt-2 p-3 text-[#9241C8] text-xs font-semibold bg-[#ede5f2] rounded-xl rounded-bl-md shadow-md">
+                That sounds great. I’d be happy to discuss more.
+              </p>
+              <p className="w-2/3 ml-3 mt-2 p-3 text-[#9241C8] text-xs font-semibold bg-[#ede5f2] rounded-xl rounded-bl-md shadow-md">
+                Could you send over some pictures of your dog, please?
+              </p>
+              <div className="mt-3 flex flex-col items-end">
+                <div className="flex justify-end">
+                  <img src={Pic1} alt="" className="w-16 h-16 mr-2 rounded-xl" />
+                  <img src={Pic2} alt="" className="w-16 h-16 mr-2 rounded-xl" />
+                  <img src={Pic3} alt="" className="w-16 h-16 mr-2 rounded-xl" />
+                </div>
+                <p className="w-2/3 mr-3 mt-2 p-3 text-[#6E5D7E] text-xs bg-white rounded-xl rounded-br-md shadow-md">
+                  Here are a few pictures. She’s a happy girl!
+                </p>
+                <p className="w-2/5 mr-3 mt-2 p-3 text-[#6E5D7E] text-xs bg-white rounded-xl rounded-br-md shadow-md">
+                  Can you make it?
+                </p>
+              </div>
+              <div>
+                <p className="w-2/3 ml-3 mt-2 p-3 text-[#9241C8] text-xs font-semibold bg-[#ede5f2] rounded-xl rounded-bl-md shadow-md">
+                  She looks so happy! The time we discussed works. How long shall
+                  I take her out for?
+                </p>
+                <div className="w-4/5 ml-3 mt-2 py-2 px-4 font-rubik flex justify-between text-white bg-gradient-to-r from-[#E942FF] to-[#8838FF] rounded-2xl">
+                  <div className="flex items-center">
+                    <div className="w-5 h-5 border-2 border-white rounded-full"></div>
+                    <p className="ml-2 text-xs">30 minute walk</p>
+                  </div>
+                  <p className="font-extrabold text-xl">$29</p>
+                </div>
+                <div className="w-4/5 ml-3 mt-2 py-2 px-4 font-rubik flex justify-between text-white bg-gradient-to-r from-[#E942FF] to-[#8838FF] rounded-2xl">
+                  <div className="flex items-center">
+                    <div className="w-5 h-5 border-2 border-white rounded-full"></div>
+                    <p className="ml-2 text-xs">30 minute walk</p>
+                  </div>
+                  <p className="font-extrabold text-xl">$29</p>
+                </div>
+              </div>
+              <div className="w-[17rem] flex justify-between items-center mt-2 mx-auto px-1 py-1 bg-white rounded-full">
+                <input
+                  type="text"
+                  placeholder="Type a message..."
+                  className="pl-3"
+                />
+                <GrFormNext className="icon bg-[#3E2753] w-9 h-9 rounded-full" />
+              </div>
+            </div>
+          </section>
+
+          <section className="font-rubik z-10 text-center lg:ml-24 lg:text-start">
+            <p className="text-4xl text-[#3E2753] font-medium py-10">
+              Simple booking
+            </p>
+            <p className="w-[20rem] lg:w-[30rem] text-[#A39DAA] leading-7 tracking-wider font-normal">
+              Stay in touch with our dog walkers through the chat interface. This
+              makes it easy to discuss arrangements and make bookings. Once the
+              walk has been completed you can rate your walker and book again all
+              through the chat.
+            </p>
+          </section>
+        </div>
+        <div className="absolute -top-44 lg:top-[-19rem] -left-36 w-[20rem] lg:w-[33rem] h-[40rem] lg:h-[60rem] rounded-[10rem] lg:rounded-[17rem] bg-gradient-to-b from-[#E942FF] to-[#8838FF]"></div>
+        <div className="absolute -bottom-[22rem] lg:bottom-[-20rem] -right-36 lg:right-[-13rem] w-[20rem] lg:w-[33rem] h-[50rem] lg:h-[60rem] rounded-[10rem] lg:rounded-[17rem] bg-[#f9f4fa]"></div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/assets/design/style-guide.md
@@ -0,0 +1,46 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+#### Text
+
+- Pale Violet (sub-heading at the top of the app UI): hsl(276, 100%, 81%) - #D89EFF
+- Moderate Violet (chat on the left): hsl(276, 55%, 52%) - #9241C8
+- Desaturated Dark Violet (chat on the right): hsl(271, 15%, 43%) - #6E5D7E
+- Grayish Blue (placeholder text): hsl(206, 6%, 79%) - #C6CACD
+- Very Dark Desaturated Violet (main heading): hsl(271, 36%, 24%) - #3E2753
+- Dark Grayish Violet (paragraph): hsl(270, 7%, 64%) - #A39DAA
+
+#### Gradients
+
+These two colors are the vibrant background colors you see throughout the design and are applied as a linear gradient:
+
+- Light Magenta: hsl(293, 100%, 63%) - #E942FF
+- Light Violet: hsl(264, 100%, 61%) - #8838FF
+
+### Secondary
+
+- White: hsl(0, 0%, 100%)
+- Light Grayish Violet (app background): hsl(270, 20%, 96%) - #F5F3F7
+- Very Dark Desaturated Violet (submit button background): hsl(271, 36%, 24%) - #3E2753
+- Very Light Magenta (radio button outline): hsl(289, 100%, 72%) - #E570FF
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Font
+
+- Family: [Rubik](https://fonts.google.com/specimen/Rubik)
+- Weights: 400, 500, 700
